cc2413572b6a65a92df2fdc5a81852fcac8ab33f
aravindh203 <aravindhkumar218@gmail.com>
2022-11-29 16:53:12 +0530

    add a new files

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$('.testimonial-content').owlCarousel({
+    center: true,
+	autoplay:true,
+    items:1,
+    loop:true,
+    margin:50,
+    responsive:{
+		0:{
+			items:1
+		},
+		600:{
+			items:1
+		},
+        1000:{
+            items:3
+        }
+    }
+});
+$('.event-carousel').owlCarousel({
+    center: true,
+	autoplay:true,
+    items:1,
+    loop:true,
+    margin:0,
+    responsive:{
+		0:{
+			items:1
+		},
+		600:{
+			items:1
+		},
+        1000:{
+            items:1
+        }
+    }
+});
